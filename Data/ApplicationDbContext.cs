e4825485b73027d657398ac5e89373bb68a9df86
Ove Sundal <osundal83@gmail.com>
2023-04-22 21:48:59 +0200

    Add database and add migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Vinlotteri_backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Lottery> Lotteries { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<Wine> Wines { get; set; }
+}
--- Models/Lottery.cs
+++ Models/Lottery.cs
@@ -1,9 +1,13 @@
-﻿namespace Vinlotteri_backend.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vinlotteri_backend.Data;
 
 public class Lottery
 {
-    public int Id { get; init; }
-    public decimal TicketPrice { get; init; }
-    public int TotalTickets { get; init; }
-    public int TicketsSold { get; init; }
+    [Key] public int Id { get; init; }
+    [Required] public decimal TicketPrice { get; init; }
+    [Required] public int TotalTickets { get; init; }
+    [Required] public int TicketsSold { get; init; }
+    public ICollection<Ticket> Tickets { get; set; }
+    public ICollection<Wine> Prizes { get; set; }
 }
