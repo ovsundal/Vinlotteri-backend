fcbfc28f604c68cdd2cd2726a77db814d59d7c3c
Ove Sundal <osundal83@gmail.com>
2023-04-25 21:43:37 +0200

    add migration

---
+++ Migrations/20230425194041_AddHasBeendAwardedPropertyToWineTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vinlotteri_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHasBeendAwardedPropertyToWineTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenAwarded",
+                table: "Wines",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasBeenAwarded",
+                table: "Wines");
+        }
+    }
+}
