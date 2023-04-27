cb80c33cf06ee433ea5e813ec164b74aa6ba05cd
Ove Sundal <osundal83@gmail.com>
2023-04-27 21:44:38 +0200

    update property setter

--- Models/Wine.cs
+++ Models/Wine.cs
@@ -8,7 +8,7 @@ public class Wine
     [Key] public int Id { get; init; }
     [Required] public decimal Price { get; init; }
     [Required] public string Name { get; init; }
-    [Required] public string WonBy { get; init; }
+    [Required] public string WonBy { get; set; }
     [ForeignKey("Lottery")] public int LotteryId { get; set; }
     public Lottery Lottery { get; set; }
 }
