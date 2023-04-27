f15718149a2152327109a524cda3fbea883ae3d7
Ove Sundal <osundal83@gmail.com>
2023-04-27 21:20:39 +0200

    add migration and update database

---
+++ Migrations/20230427191847_ChangeWinePropertyType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vinlotteri_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeWinePropertyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasBeenAwarded",
+                table: "Wines");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WonBy",
+                table: "Wines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WonBy",
+                table: "Wines");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenAwarded",
+                table: "Wines",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
