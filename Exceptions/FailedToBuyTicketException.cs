739101f2da80704df19b8cbe31d38546794a74b4
Ove Sundal <osundal83@gmail.com>
2023-08-13 21:22:31 +0200

    add FailedToBuyTicketException

---
+++ Exceptions/FailedToBuyTicketException.cs
@@ -0,0 +1,8 @@
+﻿namespace Vinlotteri_backend.Exceptions;
+
+public class FailedToBuyTicketException : Exception
+{
+        public FailedToBuyTicketException() : base("Failed to buy ticket")
+    {
+    }
+} 
