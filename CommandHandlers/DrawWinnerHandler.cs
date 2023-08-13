3e724ea34c442219d461e7bf611e278a20d9e198
Ove Sundal <osundal83@gmail.com>
2023-08-13 21:40:52 +0200

    add command and handler for DrawWinner

---
+++ CommandHandlers/DrawWinnerHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Vinlotteri_backend.Commands;
+using Vinlotteri_backend.DTOs;
+using Vinlotteri_backend.Exceptions;
+using Vinlotteri_backend.Services;
+
+namespace Vinlotteri_backend.CommandHandlers;
+
+public class DrawWinnerHandler : IRequestHandler<DrawWinnerCommand, LotteryDto?>
+{
+    private readonly ILotteryService _lotteryService;
+
+    public DrawWinnerHandler(ILotteryService lotteryService)
+    {
+        _lotteryService = lotteryService;
+    }
+    
+    public async Task<LotteryDto?> Handle(DrawWinnerCommand request, CancellationToken cancellationToken)
+    {
+        var success = await _lotteryService.DrawWinner(request.LotteryId, request.WineId);
+
+        if (!success)
+        {
+            throw new FailedToDrawWinnerException();
+        }
+    
+        return await _lotteryService.GetLotteryById(request.LotteryId);
+    }
+}
---
+++ Commands/DrawWinnerCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Vinlotteri_backend.DTOs;
+
+namespace Vinlotteri_backend.Commands;
+
+public class DrawWinnerCommand : IRequest<LotteryDto>
+{
+    public required int LotteryId { get; init; }
+    public required int WineId { get; init; }
+}
